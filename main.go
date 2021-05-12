6e01f7ee7c4c77c57731aaae9687177b651faeed
Joe <doncrafttv@gmail.com>
2021-05-12 15:39:05 +0300

    part of insert

--- main.go
+++ main.go
@@ -2,30 +2,105 @@ package main
 
 import (
 	"./mercator"
+	"bufio"
 	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
-type rect struct {
-	minLatitude  float64
-	minLongitude float64
-	maxLatitude  float64
-	maxLongitude float64
-	subRects     []*rect
-	nodes        []*node
-}
+const maxNodes = 10
 
 type node struct {
-	latitude    float64 //Географічна широта
-	longitude   float64 //Географічна довгота
-	nodeType    string  //Тип
-	nodeSubType string  //Підтип
-	address     string  //Адреса
+	coords        mercator.Coords
+	nodeType      string //Тип
+	nodeSubType   string //Підтип
+	name          string //Назва
+	address       string //Адреса
+	addressNumber string //Адреса
+}
+
+func (n *node) parseCSV(str string) {
+	sl := strings.Split(str, ";")
+	//Get coords
+	Lat, err := strconv.ParseFloat(sl[0], 64)
+	if err != nil {
+		log.Fatal("Invalid data:", sl[0], "is not a float value")
+	}
+	Long, err := strconv.ParseFloat(sl[1], 64)
+	if err != nil {
+		log.Fatal("Invalid data:", sl[1], "is not a float value")
+	}
+	geo := mercator.GeoCoords{
+		Latitude:  Lat,
+		Longitude: Long,
+	}
+	n.coords = geo.ToMercator()
+	n.nodeType = sl[2]
+	n.nodeSubType = sl[3]
+	n.name = sl[4]
+	n.address = sl[5]
+	n.addressNumber = sl[6]
+}
+
+type rect struct {
+	minCoords mercator.Coords // Нижня ліва частина прямокутника
+	maxCoords mercator.Coords // Верхня права частина прямокутника
+	subRects  []*rect
+	nodes     []node
+}
+
+func (head *rect) area() float64 {
+	return math.Abs((head.maxCoords.Y - head.minCoords.Y) * (head.maxCoords.X - head.minCoords.X))
+}
+
+func (head *rect) resize(newNode node) {
+	head.minCoords.GetSmallest(newNode.coords, len(head.nodes) == 0)
+	head.maxCoords.GetBiggest(newNode.coords, len(head.nodes) == 0)
+}
+
+func (head *rect) insert(newNode node) {
+	if len(head.subRects) != 0 {
+		var minId = 0
+		var minArea = head.subRects[0].area()
+		var curArea = head.area()
+		for i, v := range head.subRects {
+			if curArea < minArea {
+				minId = i
+				minArea = v.area()
+			}
+		}
+		head.subRects[minId].insert(newNode)
+		return
+	}
+	if len(head.nodes) == maxNodes {
+		//Split
+	}
+
+	head.minCoords.GetSmallest(newNode.coords, len(head.nodes) == 0)
+	head.maxCoords.GetBiggest(newNode.coords, len(head.nodes) == 0)
+
+	head.nodes = append(head.nodes, newNode)
+}
+
+func () {
+
 }
 
 func main() {
-	a := mercator.Coords{
-		Latitude:  55.751667,
-		Longitude: 37.617778,
+	file, err := os.Open("./src/ukraine_poi.csv")
+	if err != nil {
+		log.Fatal("No such file")
 	}
-	fmt.Println(a.ToMercator())
+	var head rect
+	reader := bufio.NewScanner(file)
+	for i := 0; i < 5; i++ {
+		var newNode node
+		reader.Scan()
+		newNode.parseCSV(reader.Text())
+		head.insert(newNode)
+		fmt.Println(head.minCoords, head.maxCoords)
+	}
 }
--- mercator/mercator.go
+++ mercator/mercator.go
@@ -2,16 +2,39 @@ package mercator
 
 import "math"
 
-type Coords struct {
+type GeoCoords struct {
 	Latitude  float64
 	Longitude float64
+}
+
+type Coords struct {
+	X float64
+	Y float64
+}
+
+func (c *Coords) GetSmallest(newCoords Coords, isNull bool) {
+	if c.X > newCoords.X || isNull {
+		c.X = newCoords.X
+	}
+	if c.Y > newCoords.Y || isNull {
+		c.Y = newCoords.Y
+	}
+}
+
+func (c *Coords) GetBiggest(newCoords Coords, isNull bool) {
+	if c.X < newCoords.X || isNull {
+		c.X = newCoords.X
+	}
+	if c.Y < newCoords.Y || isNull {
+		c.Y = newCoords.Y
+	}
 }
 
 const a = 6378137
 const b = 6356752.3142
 const f = (a - b) / a
 
-func (c *Coords) Radian() (float64, float64) {
+func (c *GeoCoords) Radian() (float64, float64) {
 	return toRadian(c.Latitude), toRadian(c.Longitude)
 }
 
@@ -23,9 +46,10 @@ func e() float64 {
 	return math.Sqrt(2*f - math.Pow(f, 2))
 }
 
-func (c *Coords) ToMercator() (float64, float64) {
+func (c *GeoCoords) ToMercator() Coords {
+	var newCoords Coords
 	lat, long := c.Radian()
-	mercY := a * long
-	mercX := a * math.Log(math.Tan(math.Pi/4+lat/2)*math.Pow((1-e()*math.Sin(lat))/(1+e()*math.Sin(lat)), e()/2))
-	return mercX, mercY
+	newCoords.Y = a * long
+	newCoords.X = a * math.Log(math.Tan(math.Pi/4+lat/2)*math.Pow((1-e()*math.Sin(lat))/(1+e()*math.Sin(lat)), e()/2))
+	return newCoords
 }
